d5765d98ddbd348cbb420a6a8f4ca5cda7d1c0f2
Plinkett <carlos.santillan.vasquez@gmail.com>
2022-09-06 11:46:11 +0200

    update: finished writing classification script

--- code/classification.R
+++ code/classification.R
@@ -4,6 +4,8 @@ library(mvtnorm)
 library(MASS)
 library(class)
 library(e1071)
+library(dplyr)
+library(ggplot2)
 
 setwd("~/Documents/GitHub/wifi-traffic-localization/data/second_capture/processed")
 
@@ -26,8 +28,10 @@ boxplot(notype, col = 'yellow')
 
 # The data isn't centered, we center the data around its mean
 notype <- scale(notype, scale = FALSE)
-boxplot(notype, col = 'yellow')
-
+pdf("~/plot.pdf", width=10,height=7)
+par(mar = c(7, 7, 3, 7))
+boxplot(notype, col = 'yellow', las =2)
+dev.off()
 # We standardize the data because of the variance of one of the features
 notype <- scale(notype)
 boxplot(notype, col = 'yellow')
@@ -55,7 +59,7 @@ for(i in 1:4) barplot(pc$loadings[,i], ylim = c(-1, 1), main=paste("PC",i), col 
 # We see that they are mostly weighted means and contrasts between features
 
 X11()
-plot(pc$scores[,1:2], col=dataset$type_of_traffic, pch=19)
+plot(pc$scores[,2:3], col=dataset$type_of_traffic, pch=19)
 
 # We plot the pairs and color accordingly
 
@@ -82,7 +86,7 @@ dev.off()
 # We compute the principal components and also the scores
 pc <- princomp(notype, scores = T)
 summary(pc)
-pdf()
+
 X11()
 pairs(pc$scores[,1:3], col=train$type_of_traffic, pch = 19)
 legend("topright", fill = unique(train$type_of_traffic), legend = c(levels(train$type_of_traffic)))
@@ -109,7 +113,7 @@ min(errors)
 test_scaled <- scale(test_traffic[,1:11], attr(notype, "scaled:center"), attr(notype, "scaled:scale"))
 
 # We "train" KNN
-traffic.knn <- knn(train = notype, test = test_scaled, cl = train$type_of_traffic, k = 4)
+traffic.knn <- knn(train = notype, test = test_scaled, cl = train$type_of_traffic, k = 6)
 traffic.knn
 
 
@@ -130,7 +134,7 @@ pc2  <- seq(min(scores_1_3[,2]), max(scores_1_3[,2]), length=300)
 
 #xyz <- expand.grid(xcoord=pc1, ycoord=pc2, zcoord=pc3)
 xy <- expand.grid(x = pc1, y = pc2)
-contour.knn <- knn(train = scores_1_3[,1:2], test = xy, cl = train$type_of_traffic, k = 4)
+contour.knn <- knn(train = scores_1_3[,1:2], test = xy, cl = train$type_of_traffic, k = 6)
 z  <- as.numeric(contour.knn)
 
 
@@ -138,3 +142,55 @@ contour(pc1, pc2, matrix(z, 300), levels=c(1.5, 2.5), drawlabels=F, add=T)
 
 graphics.off()
 dev.off()
+
+
+
+confusion <- data.frame(true = test_traffic$type_of_traffic,
+                     predicted = traffic.knn)
+X11()
+
+conf_matrix(confusion$true, confusion$predicted, title = "Conf. Matrix Example")
+
+conf_matrix <- function(df.true, df.pred, title = "", true.lab ="True Class", pred.lab ="Predicted Class",
+                        high.col = 'red', low.col = 'white') {
+  #convert input vector to factors, and ensure they have the same levels
+  df.true <- as.factor(df.true)
+  df.pred <- factor(df.pred, levels = levels(df.true))
+  
+  #generate confusion matrix, and confusion matrix as a pecentage of each true class (to be used for color) 
+  df.cm <- table(True = df.true, Pred = df.pred)
+  df.cm.col <- df.cm / rowSums(df.cm)
+  
+  #convert confusion matrices to tables, and binding them together
+  df.table <- reshape2::melt(df.cm)
+  df.table.col <- reshape2::melt(df.cm.col)
+  df.table <- left_join(df.table, df.table.col, by =c("True", "Pred"))
+  
+  #calculate accuracy and class accuracy
+  acc.vector <- c(diag(df.cm)) / c(rowSums(df.cm))
+  class.acc <- data.frame(Pred = "Class Acc.", True = names(acc.vector), value = acc.vector)
+  acc <- sum(diag(df.cm)) / sum(df.cm)
+  
+  #plot
+  ggplot() +
+    geom_tile(aes(x=Pred, y=True, fill=value.y),
+              data=df.table, size=0.2, color=grey(0.5)) +
+    geom_tile(aes(x=Pred, y=True),
+              data=df.table[df.table$True==df.table$Pred, ], size=1, color="black", fill = 'transparent') +
+    scale_x_discrete(position = "top",  limits = c(levels(df.table$Pred), "Class Acc.")) +
+    scale_y_discrete(limits = rev(unique(levels(df.table$Pred)))) +
+    labs(x=pred.lab, y=true.lab, fill=NULL,
+         title= paste0(title, "\nAccuracy ", round(100*acc, 1), "%")) +
+    geom_text(aes(x=Pred, y=True, label=value.x),
+              data=df.table, size=4, colour="black") +
+    geom_text(data = class.acc, aes(Pred, True, label = paste0(round(100*value), "%"))) +
+    scale_fill_gradient(low=low.col, high=high.col, labels = scales::percent,
+                        limits = c(0,1), breaks = c(0,0.5,1)) +
+    guides(size=F) +
+    theme_bw() +
+    theme(panel.border = element_blank(), legend.position = "bottom",
+          axis.text = element_text(color='black'), axis.ticks = element_blank(),
+          panel.grid = element_blank(), axis.text.x.top = element_text(angle = 30, vjust = 0, hjust = 0)) +
+    coord_fixed()
+  
+} 
